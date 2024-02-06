9f6c0b951e9f9c3bf4c1f352ebd4ca0d7398e76f
vanessaPrankl <vanessa.prankl@gmail.com>
2024-02-06 17:43:15 +0100

    Finished LSM6DSO IMU test - everything working

--- Firmware/HeadMouse-V1-hardware-test/src/main_imu_test.cpp
+++ Firmware/HeadMouse-V1-hardware-test/src/main_imu_test.cpp
@@ -10,12 +10,76 @@
 #include "SparkFunLSM6DSO.h"
 
 /* PIN DEFINITIONS ******************************************************/
-
 const int8_t PIN_I2C_SCL = 9;
 const int8_t PIN_I2C_SDA = 10;
 const int8_t PIN_LSM6DS_INT = 11;
 const int8_t PIN_MC6470_INT = 12;
 const int8_t PIN_BNO55_INT = 13;
+
+LSM6DSO lsm6dso;
+
+
+/* FUNCTION PROTOTYPES **************************************************/
+void i2c_bus_scan();
+
+
+/* INIT *****************************************************************/
+void setup() {
+
+  Serial.begin(9600);
+
+  /* Init I2C */
+  Wire.begin(PIN_I2C_SDA, PIN_I2C_SCL, 400000);
+  delay(10);  /* Let I2C bus come up */
+
+  /* Init LSM6DSO IMU */
+  if(lsm6dso.begin(0x6A, Wire)){
+    Serial.println("LSM6DSO ready");
+  }
+  else{
+    Serial.println("LSM6DSO not reachable");
+  }
+
+  if(lsm6dso.initialize(BASIC_SETTINGS)){
+    Serial.println("LSM6DSO initialized");
+  }
+  else{
+    Serial.println("Canno init LSM6DSO");
+  }
+
+  delay(5000); 
+
+  i2c_bus_scan();
+}
+
+
+/* MAIN ******************************************************************/
+void loop() {
+  
+  /* Read LSM6DSO IMU params */
+  Serial.print("\nAccelerometer:\n");
+  Serial.print(" X = ");
+  Serial.println(lsm6dso.readFloatAccelX(), 3);
+  Serial.print(" Y = ");
+  Serial.println(lsm6dso.readFloatAccelY(), 3);
+  Serial.print(" Z = ");
+  Serial.println(lsm6dso.readFloatAccelZ(), 3);
+
+  Serial.print("\nGyroscope:\n");
+  Serial.print(" X = ");
+  Serial.println(lsm6dso.readFloatGyroX(), 3);
+  Serial.print(" Y = ");
+  Serial.println(lsm6dso.readFloatGyroY(), 3);
+  Serial.print(" Z = ");
+  Serial.println(lsm6dso.readFloatGyroZ(), 3);
+
+  Serial.print("\nThermometer:\n");
+  Serial.print(" Degrees F = ");
+  Serial.println(lsm6dso.readTempF(), 3);
+
+  delay(1000);       
+}
+
 
 /* FUNCTION DEFINITIONS **************************************************/
 void i2c_bus_scan(){
@@ -46,25 +110,3 @@ void i2c_bus_scan(){
   if (nDevices == 0) Serial.println("No I2C devices found\n");
   else Serial.println("I2C scan finished\n");
 }
-
-
-/* INIT *****************************************************************/
-void setup() {
-
-  Serial.begin(9600);
-
-  /* Init I2C */
-  Wire.begin(PIN_I2C_SDA, PIN_I2C_SCL, 400000);
-
-  delay(5000); 
-}
-
-/* MAIN ******************************************************************/
-void loop() {
-
-  i2c_bus_scan();
-
-  delay(1000);       
-}
-
-
