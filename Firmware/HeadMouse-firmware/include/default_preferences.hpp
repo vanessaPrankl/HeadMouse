ecb1b22e8151be150f5038d8953a4f60be2d0579
vanessaPrankl <vanessa.prankl@gmail.com>
2024-06-27 17:47:16 +0200

    Added default preferences for device config

--- Firmware/HeadMouse-firmware/include/default_preferences.hpp
+++ Firmware/HeadMouse-firmware/include/default_preferences.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
-/* LOG-LEVELS ***************************************************/
-#define LOG_LEVEL_DEBUG    1
-#define LOG_LEVEL_INFO     1
-#define LOG_LEVEL_WARNING  1
-#define LOG_LEVEL_ERROR    1
+#include "preferences.hpp"
+#include "hm_board_config_v1_0.hpp"
 
-/* COMMUNICATION *************************************************/
-constexpr uint32_t SERIAL_BAUD_RATE = 115200;
+/* LOG-LEVELS ***************************************************/
+#define LOG_LEVEL_DEBUG     1
+#define LOG_LEVEL_DEBUG_BAT 1
+#define LOG_LEVEL_INFO      1
+#define LOG_LEVEL_WARNING   1
+#define LOG_LEVEL_ERROR     1
+
+/* DEVICE CONFID DEFAULTS ********************************************/
+constexpr devMode HM_DEF_MODE = RELATIVE;
+constexpr devSensitivity HM_DEF_SENSITIVITY = SENSITIVITY_4;
+constexpr btnAction HM_DEF_ACTION_BTN_1 = MOUSE_RIGHT;
+constexpr btnAction HM_DEF_ACTION_BTN_2 = MOUSE_LEFT;
+constexpr btnAction HM_DEF_ACTION_BTN_3 = MOUSE_WHEEL;
+constexpr btnAction HM_DEF_ACTION_BTN_4 = BLE_CONNECTION;
+
